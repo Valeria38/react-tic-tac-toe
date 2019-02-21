899099454645a0d12a9be47aec180a349859824f
Valeria Gora <valeriia308@gmail.com>
2019-02-21 13:43:07 +0300

    Updates

---
+++ precache-manifest.f34a1c02371c4ef7ca35d8e05aa7920b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7d1b9009599586221ce9",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.7d1b9009.js"
+  },
+  {
+    "revision": "cd2bed96230d028c4bb5",
+    "url": "/react-tic-tac-toe/static/js/main.cd2bed96.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-tic-tac-toe/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "cd2bed96230d028c4bb5",
+    "url": "/react-tic-tac-toe/static/css/main.65b5a6b9.chunk.css"
+  },
+  {
+    "revision": "deab99c5d0a7f846e66bba77ad79aa4c",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
